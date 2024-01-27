a5e2eb42cd649eb8f086fc61a743a5e2bb0b3fc5
jikael <jikaelgagnon@gmail.com>
2024-01-27 16:41:54 -0500

    ggg

--- __init__.py
+++ __init__.py
@@ -1,12 +1,6 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-import os
 
 # this file causes this folder to act as a package, making imports easier
 app = Flask(__name__)
-app.app_context().push() # Needed to create db
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-
-db = SQLAlchemy(app)
 
 from crime_pathfinder import routes
