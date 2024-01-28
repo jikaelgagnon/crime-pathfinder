11dc0add1da12b522c4e388bb7e14ea49a452328
cosmicbkyle <benjaminkkyle@gmail.com>
2024-01-27 20:07:09 -0500

    Removed useless files

--- ben_test.py
+++
@@ -1,4 +0,0 @@
-import urllib
-url = 'https://donnees.montreal.ca/api/3/action/datastore_search?resource_id=c6f482bf-bf0f-4960-8b2f-9982c211addd&limit=5&q=title:jones'  
-fileobj = urllib.urlopen(url)
-print(fileobj.read())
