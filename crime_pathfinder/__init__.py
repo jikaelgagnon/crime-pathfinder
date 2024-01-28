e6328fdf6bd1745fccf27e456c6ebbc54921ef1f
jikael <jikaelgagnon@gmail.com>
2024-01-27 22:43:06 -0500

    fixed merges with Jan

--- crime_pathfinder/__init__.py
+++ crime_pathfinder/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 import os
 
 # this file causes this folder to act as a package, making imports easier
@@ -6,5 +7,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'banana'
 app.app_context().push() # Needed to create db
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+datalist = []
 
 from crime_pathfinder import routes
--- crime_pathfinder/routes.py
+++ crime_pathfinder/routes.py
@@ -1,28 +1,26 @@
 from flask import Flask, render_template, request, url_for, flash, redirect, render_template_string
 from crime_pathfinder import app
 from crime_pathfinder.forms import MapForm
-from crime_pathfinder.map_utils import generate_map
+from crime_pathfinder.map_utils import get_df, get_map
 import json
 import plotly
 import plotly.express as px
 import pandas as pd
+from crime_pathfinder.texttolonlat import geocode_address
+from crime_pathfinder import datalist
 # app = Flask(__name__)
 app.config['SECRET_KEY'] = 'df0331cefc6c2a9a5d0208a726a5d1c0fd17324feba25506'
-
-messages = [{'title': 'Message One',
-             'content': 'Message One Content'},
-            {'title': 'Message Two',
-             'content': 'Message Two Content'}
-            ]
 
 @app.route('/', methods=('GET', 'POST'))
 def index():
     form = MapForm()
+    print(datalist)
     if request.method == "POST" and form.validate_on_submit():
         flash('Map data submitted succesfully!', 'success')
         categories = form.categories.data
         times_of_day = form.times_of_day.data
-        fig = generate_map(2024,categories=categories,times_of_day=times_of_day)
+        df = get_df(2024,categories=categories,times_of_day=times_of_day)
+        fig = get_map(df)
         div = fig.to_html(full_html=False)
         return render_template_string('''
             <head>
@@ -38,15 +36,22 @@ def index():
 @app.route('/create/', methods=('GET', 'POST'))
 def create():
     if request.method == 'POST':
-        title = request.form['title']
-        content = request.form['content']
+        crime_1 = request.form['Crime']
+        location = request.form['Location']
+        description_1 = request.form['Description']
+        time_of_day = request.form['Time']
 
-        if not title:
-            flash('Title is required!')
-        elif not content:
-            flash('Content is required!')
+        coordonates = geocode_address('AIzaSyB0OTXJmDBC3Al_FiDhMojWLK9F8Obm5x8', location)
+        coordonates_lat = coordonates[0]
+        coordonates_long = coordonates[1]
+
+
+        if not crime_1:
+            flash('Crime type is required!')
+        elif not location:
+            flash('Location is required!')
         else:
-            messages.append({'title': title, 'content': content})
+            datalist.append({'crime': crime_1, 'description': description_1, 'Latitude': coordonates_lat, 'Longitude': coordonates_long, 'time_of_day': time_of_day})
             return redirect(url_for('index'))
-
+    
     return render_template('create.html')
