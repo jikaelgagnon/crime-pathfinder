9e17be63f3d4f51c40a0e9fcd88f147f77bd0dcb
Jan van heusde <janvheusde@gmail.com>
2024-01-27 23:31:40 -0500

    mucho chango

--- crime_pathfinder/routes.py
+++ crime_pathfinder/routes.py
@@ -1,31 +1,46 @@
 from flask import Flask, render_template, request, url_for, flash, redirect
-from crime_pathfinder import app
+from datetime import datetime
+from crime_pathfinder import app , db , datalist
+from flask_sqlalchemy import SQLAlchemy
+from crime_pathfinder.database import Incident
+from crime_pathfinder.texttolonlat import geocode_address
 
-# app = Flask(__name__)
 app.config['SECRET_KEY'] = 'df0331cefc6c2a9a5d0208a726a5d1c0fd17324feba25506'
-
-messages = [{'title': 'Message One',
-             'content': 'Message One Content'},
-            {'title': 'Message Two',
-             'content': 'Message Two Content'}
-            ]
 
 @app.route('/')
 def index():
-    return render_template('index.html', messages=messages)
+    return render_template('index.html')
 
 @app.route('/create/', methods=('GET', 'POST'))
 def create():
     if request.method == 'POST':
-        title = request.form['title']
-        content = request.form['content']
+        crime_1 = request.form['Crime']
+        location = request.form['Location']
+        description_1 = request.form['Description']
+        time_of_day = request.form['Time']
 
-        if not title:
-            flash('Title is required!')
-        elif not content:
-            flash('Content is required!')
+        coordonates = geocode_address('AIzaSyB0OTXJmDBC3Al_FiDhMojWLK9F8Obm5x8', location)
+        coordonates_lat = coordonates[0]
+        coordonates_long = coordonates[1]
+
+
+        if not crime_1:
+            flash('Crime type is required!')
+        elif not location:
+            flash('Location is required!')
         else:
-            messages.append({'title': title, 'content': content})
+            datalist.append({'crime': crime_1, 'description': description_1, 'Latitude': coordonates_lat, 'Longitude': coordonates_long, 'time_of_day': time_of_day})
             return redirect(url_for('index'))
-
+    
     return render_template('create.html')
+
+
+ #crime_report = Incident(
+           # crime_type = crime_1,
+           # description = description_1,
+           # longitude = coordonates_long,
+           # latitude = coordonates_lat
+        #)
+            
+           # db.session.add(crime_report)
+           # db.session.commit()
